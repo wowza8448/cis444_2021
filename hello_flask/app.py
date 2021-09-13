fc289f6f829f35496b68a6a54f6a81d6d73400de
Ubuntu <cjardin@jardinlabs.com>
2021-09-13 21:31:11 +0000

    added Static

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -2,9 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+USER_PASSWORDS = { "cjardin": "strong password"}
+
+
+@app.route('/') #endpoint
 def index():
-    return 'Web App with Python Flask!'
+    return 'Web App with Python Caprice!' + USER_PASSWORDS['cjardin']
+
+@app.route('/buy') #endpoint
+def buy():
+    return 'Buy'
 
 app.run(host='0.0.0.0', port=80)
 
