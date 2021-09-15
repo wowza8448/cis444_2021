895131c2d9f13e83ff75956b1d7a87d75dbf7b84
Ubuntu <cjardin@jardinlabs.com>
2021-09-15 21:18:26 +0000

    Lecture 09.15.2021

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 USER_PASSWORDS = { "cjardin": "strong password"}
 
+IMGS_URL = {
+            "DEV" : "/static",
+            "INT" : "https://cis-444-fall-2021.s3.us-west-2.amazonaws.com/images",
+            "PRD" : "http://d2cbuxq67vowa3.cloudfront.net/images"
+            }
+
+CUR_ENV = "PRD"
 
 @app.route('/') #endpoint
 def index():
@@ -13,6 +20,10 @@ def index():
 @app.route('/buy') #endpoint
 def buy():
     return 'Buy'
+
+@app.route('/hello') #endpoint
+def hello():
+    return render_template('hello.html',img_url=IMGS_URL[CUR_ENV] ) 
 
 app.run(host='0.0.0.0', port=80)
 
