e8d866a68d19e7edd94efee879cfc561a5204233
Ubuntu <ubuntu@ip-172-31-22-191.us-west-1.compute.internal>
2021-10-25 02:04:41 +0000

    HW3 createUsers

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -48,12 +48,24 @@ def backp():
 
 app.config['SECRET_KEY'] = 'supersecretkey'
 
+
 @app.route('/getUser', methods =["GET", "POST"])
 def getUser():
     if request.method == "POST":
+        cur = global_db_con.cursor()
         username = request.form.get("username")
         password = request.form.get("password")
-        return "Welcome " + username
+        cur.execute("SELECT * FROM users WHERE username = username;")
+        match = cur.fetchone()
+        if match == None:
+            return "Invalid login"
+        else:
+            hash = match[1].encode('utf-8')
+            new = bcrypt.checkpw(bytes(password, 'utf-8'), hash)
+            if True:
+                return "Login accepted"
+            else:
+                return "Invalid login"
     return render_template("first_form.html")
 
 @app.route('/addUser', methods = ["GET", "POST"])
@@ -62,15 +74,17 @@ def addUser():
         cur = global_db_con.cursor()
         username1 = request.form.get("username")
         password = request.form.get("password")
-        payload = list(password)
-        password1 = jwt.encode({'password': password}, app.config['SECRET_KEY'])
+       # payload = list(password)
+       # password1 = jwt.encode({'password': password}, app.config['SECRET_KEY'])
+        password = bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+        password = password.decode('utf-8')
+        print(password)
         sqlInsert = """INSERT INTO users(username, password) values(%s, %s);"""
-        cur.execute(sqlInsert,(username1, password1))
+        cur.execute(sqlInsert,(username1, password))
         global_db_con.commit()
         print("User created")
         return "User " + username1 + " created!"
     return render_template("second_form.html")
-
 
 
 #Assigment 2
