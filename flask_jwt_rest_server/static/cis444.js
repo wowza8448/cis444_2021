fb40377a9bfce822b514a385bb3c1f5a414f6137
Ubuntu <cjardin@jardinlabs.com>
2021-11-08 00:31:10 +0000

    Assigment 4, production ready REST server

---
+++ flask_jwt_rest_server/static/cis444.js
@@ -0,0 +1,19 @@
+var jwt = null
+function secure_get_with_token(endpoint, on_success_callback, on_fail_callback){
+	xhr = new XMLHttpRequest();
+	function setHeader(xhr) {
+		xhr.setRequestHeader('Authorization', 'Bearer:'+jwt);
+	}
+	function get_and_set_new_jwt(data){
+		console.log(data);
+		on_success_callback(data)
+	}
+	$.ajax({
+		url: endpoint,
+		type: 'GET',
+		datatype: 'json',
+		success: on_success_callback,
+		error: on_fail_callback,
+		beforeSend: setHeader
+	});
+}
