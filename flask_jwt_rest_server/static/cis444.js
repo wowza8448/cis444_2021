54765b1d569ac4c4ba98fbee6ef1a78659a1944f
Ubuntu <ubuntu@ip-172-31-22-191.us-west-1.compute.internal>
2021-11-19 03:58:48 +0000

    Changes to files

--- flask_jwt_rest_server/static/cis444.js
+++ flask_jwt_rest_server/static/cis444.js
@@ -1,20 +1,13 @@
-var jwt = null
-function secure_get_with_token(endpoint, on_success_callback, on_fail_callback){
-	xhr = new XMLHttpRequest();
-	function setHeader(xhr) {
-		xhr.setRequestHeader('Authorization', 'Bearer:'+jwt);
+var jwt = null;
+	function secure_get_with_token(endpoint, data_to_send, on_success_callback, on_fail_callback)
+	{
+		xhr = new XMLHttpRequest(); function setHeader(xhr) { 
+			xhr.setRequestHeader('Authorization', 'Bearer:'+jwt);
+		}
+		function get_and_set_new_jwt(data){ console.log(data); jwt = data.token 
+			on_success_callback(data)
+		}
+		$.ajax({ url: endpoint, data : data_to_send, type: 'GET', datatype: 'json', success: 
+			on_success_callback, error: on_fail_callback, beforeSend: setHeader
+		});
 	}
-	function get_and_set_new_jwt(data){
-		console.log(data);
-		jwt = data.token
-		on_success_callback(data)
-	}
-	$.ajax({
-		url: endpoint,
-		type: 'GET',
-		datatype: 'json',
-		success: on_success_callback,
-		error: on_fail_callback,
-		beforeSend: setHeader
-	});
-}
