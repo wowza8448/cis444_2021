9d99028735fe2ea25e6bbcf3e3431ee6d425fa81
Ubuntu <ubuntu@ip-172-31-22-191.us-west-1.compute.internal>
2021-12-13 23:20:16 +0000

    Changes

--- final_project/flask_jwt_rest_server/secure_calls/get_books.py
+++ final_project/flask_jwt_rest_server/secure_calls/get_books.py
@@ -9,5 +9,17 @@ from tools.logging import logger
 global_db_con = get_db()
 
 def handle_request():
-    logger.debug("Get Books Handle Request")
-    return json_response( token = create_token(  g.jwt_data ))
+    cur = global_db_con.cursor()
+    username = g.jwt_data['sub']
+    sqlExecute = "SELECT * from userchats"
+    cur.execute(sqlExecute)
+    rows = cur.fetchall()
+    if rows == None:
+        return "Chat is empty"
+    else:
+        lst = []
+        for row in rows:
+            lst.append(row[1])
+        user_books = str(lst)
+        print(user_books)
+    return json_response( token = create_token(  g.jwt_data ), pass_back = user_books, books = {})
