91c7cb311b88dcc859978b4661109d68443338c2
wowza8448 <71194678+wowza8448@users.noreply.github.com>
2021-12-13 15:56:11 -0800

    Update add_message.py

---
+++ final_project/flask_jwt_rest_server/secure_calls/add_message.py
@@ -0,0 +1,26 @@
+from flask import request, g
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+from tools.token_tools import create_token
+from db_con import get_db_instance, get_db
+import sys
+sys.path.append('..')
+
+from tools.logging import logger
+global_db_con = get_db()
+
+def handle_request():
+    cur = global_db_con.cursor()
+    username = g.jwt_data['sub']
+    sqlExecute = "SELECT * from userchats"
+    cur.execute(sqlExecute)
+    rows = cur.fetchall()
+    if rows == None:
+        return "Chat is empty"
+    else:
+        lst = []
+        for row in rows:
+            newRow = row[0] + ": " + row[1]
+            lst.append(newRow)
+        user_books = '<br>'.join(lst)
+        print(user_books)
+    return json_response( token = create_token(  g.jwt_data ), pass_back = user_books, books = {})
